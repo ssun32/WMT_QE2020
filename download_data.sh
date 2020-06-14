0f1b5c24cf4fc3b8572000d33dd5e3498e47808e
Shuo Sun <v_shuosun@tencent.com>
2020-06-14 12:32:28 -0400

    added en-ru to download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -2,6 +2,8 @@ git clone https://github.com/facebookresearch/mlqe.git
 mv mlqe/data .
 rm -rf mlqe
 cd data
+wget https://www.quest.dcs.shef.ac.uk/wmt20_files_qe/ru-en.tar.gz
+wget https://www.quest.dcs.shef.ac.uk/wmt20_files_qe/ru-en_test.tar.gz
 for f in *.tar.gz; 
 do
     tar -xvzf $f
