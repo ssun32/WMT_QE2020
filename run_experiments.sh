6454ad7abf57dbc5f6f3455392b82d64e5417af7
Adi Renduchintala <adirendu@devfair054.maas>
2020-06-25 14:08:43 -0700

    h1.05 results

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
+set -e
 num_gpus=1
+job_name="H1.05"
+for f in experiments_xlmr/${job_name}/*/*/config.json; do
+    echo $f
+    suffix="config.json"
+    scores=${f%"$suffix"}
 
-for f in experiments/*/*/*/config.json; do
-    if [[ $f == *"mtl"* ]]; then
-        python train_multitask.py --config_file $f --num_gpus=$num_gpus
-    else
-        python train.py --config_file $f --num_gpus=$num_gpus
-    fi
+    #if ls ${scores}*.test.best.scores 1> /dev/null 2>&1; then
+    #    echo "files do exist"
+    #else
+        if [[ $f == *"mtl"* ]]; then
+        srun --job-name $f --partition=priority --comment="AACl 06/26" --error ${scores}.run.stderr --output ${scores}.run.stdout --time=2880 --nodes=1 --gpus-per-node=$num_gpus --cpus-per-task 8 --constraint="volta" python train_multitask.py --config_file $f --num_gpus=$num_gpus &
+        else
+        srun --job-name $f --partition=priority --comment="AACl 06/26" --error ${scores}.run.stderr --output ${scores}.run.stdout --time=2880 --nodes=1 --gpus-per-node=$num_gpus --cpus-per-task 8 --constraint="volta" python train.py --config_file $f --num_gpus=$num_gpus &
+        fi
+    #fi
+done
